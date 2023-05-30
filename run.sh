2886d7b89ffaa514ecaac527363f8f295d75ffa0
bgreydinger <bianca.wiz@outlook.com>
2023-05-30 14:35:53 -0700

    update run

--- run.sh
+++ run.sh
@@ -13,13 +13,14 @@ iperf_port=5001
 
 for qsize in 20 100; do
     dir=bb-q$qsize
+    for protocol in [reno, bbr]:
+    	# TODO: Run bufferbloat.py here...
+    	# python3 ...
+    	python3 bufferbloat.py --dir=$dir --time=$time --bw-net=$bwnet --delay=$delay --maxq=$qsize
+    	# TODO: Ensure the input file names match the ones you use in
+    	# bufferbloat.py script.  Also ensure the plot file names match
+    	# the required naming convention when submitting your tarball.
+    	python3 plot_queue.py -f $dir/q.txt -o $protocol-buffer-q$qsize.png
+    	python3 plot_ping.py -f $dir/ping.txt -o $protocol-rtt-q$qsize.png
 
-    # TODO: Run bufferbloat.py here...
-    # python3 ...
-    python3 bufferbloat.py --dir=$dir --time=$time --bw-net=$bwnet --delay=$delay --maxq=$qsize
-    # TODO: Ensure the input file names match the ones you use in
-    # bufferbloat.py script.  Also ensure the plot file names match
-    # the required naming convention when submitting your tarball.
-    python3 plot_queue.py -f $dir/q.txt -o reno-buffer-q$qsize.png
-    python3 plot_ping.py -f $dir/ping.txt -o reno-rtt-q$qsize.png
 done
