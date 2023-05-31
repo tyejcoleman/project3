0d4e0e96d076b5ee0968d4815f7903d046761c44
bgreydinger <bianca.wiz@outlook.com>
2023-05-31 12:13:53 -0700

    update

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ for qsize in 20 100; do
 	# TODO: Ensure the input file names match the ones you use in
 	# bufferbloat.py script.  Also ensure the plot file names match
 	# the required naming convention when submitting your tarball.
-	python3 plot_queue.py -f $dir/q.txt -o $protocol-buffer-q$qsize.png
-	python3 plot_ping.py -f $dir/ping.txt -o $protocol-rtt-q$qsize.png
+	python3 plot_queue.py -f $dir/q.txt -o reno-buffer-q$qsize.png
+	python3 plot_ping.py -f $dir/ping.txt -o reno-rtt-q$qsize.png
 
 done
